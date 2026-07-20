00cbd7ab4da6ecad5f669ee5d795536ecad61482
Daniel H. <daniel.neves0021@gmail.com>
2026-07-20 10:08:13 -0300

    feat(backend): added swagger JWT setup

--- tcc_backend/Program.cs
+++ tcc_backend/Program.cs
@@ -5,15 +5,39 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+using System.Collections.Generic;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+#region Swagger
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( config => 
+{
+    config.AddSecurityDefinition(
+       "Bearer",
+        new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        });
+
+    config.AddSecurityRequirement(document => 
+        new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("Bearer", document)] = new List<string>()
+        }
+    );
+});
+#endregion
+#region Application
+builder.Services.AddControllers();
 builder.Services.AddScoped<IAuthServices, AuthServices>();
 builder.Services.AddScoped<IRDFServices, RDFServices>();
-
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -21,21 +45,21 @@ builder.Services.AddCors(options =>
         policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
 });
-
 builder.Services.Configure<RouteOptions>(options =>
 {
     options.LowercaseUrls = true;
     options.AppendTrailingSlash = true;
 });
-
+#endregion
+#region Environment
 WebApplication app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+#endregion
 
 app.UseMiddleware<AuthMiddleware>();
 app.UseHttpsRedirection();
